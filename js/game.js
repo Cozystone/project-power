7cbb23b539a24fb651a785c05b6d42d4d9e77535
anseokkim <anseokkim@gmail.com>
2025-04-02 00:18:13 +0900

    캐릭터 높이를 y축에서 z축으로 변경

--- js/game.js
+++ js/game.js
@@ -547,7 +547,7 @@ class Game {
         playerGroup.add(rightLeg);
         
         this.localPlayer = playerGroup;
-        this.localPlayer.position.set(0, 1, 1); // y좌표를 1로 올림
+        this.localPlayer.position.set(0, 0, 2); // z좌표를 2로 올림
         this.scene.add(this.localPlayer);
     }
 
